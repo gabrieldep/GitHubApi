228851df987502a0669c1d80f5d5d3e9d9b12f75
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-05-04 19:27:37 -0300

    Retirado informações desnecessárias;

--- Model/Organization.cs
+++ Model/Organization.cs
@@ -9,21 +9,7 @@ namespace GitHubApi.Model
     {
         public string login { get; set; }
         public long id { get; set; }
-        public string node_id { get; set; }
         public string avatar_url { get; set; }
-        public string gravatar_url { get; set; }
         public string url { get; set; }
-        public string html_url { get; set; }
-        public string follower_url { get; set; }
-        public string following_url { get; set; }
-        public string gists_url { get; set; }
-        public string starred_url { get; set; }
-        public string subscriptions_url { get; set; }
-        public string organizations_url { get; set; }
-        public string repos_url { get; set; }
-        public string events_url { get; set; }
-        public string received_events_url { get; set; }
-        public string type { get; set; }
-        public bool site_admin { get; set; }
     }
 }
--- Model/Repositories.cs
+++ Model/Repositories.cs
@@ -8,21 +8,10 @@ namespace GitHubApi.Model
     public class Repositories
     {
         public long id { get; set; }
-        public string node_id { get; set; }
         public string name { get; set; }
         public string full_name { get; set; }
         public Organization owner { get; set; }
-        public string html_url { get; set; }
-        public string description { get; set; }
-        public bool fork { get; set; }
-        public string language { get; set; }
-        public bool has_issues { get; set; }
-        public bool has_projects { get; set; }
-        public bool has_downloads { get; set; }
-        public bool has_wiki { get; set; }
-        public bool has_pages { get; set; }
-        public bool archived { get; set; }
-        public bool disabled { get; set; }
         public DateTime created_at { get; set; }
+        public string language { get; set; }
     }
 }
